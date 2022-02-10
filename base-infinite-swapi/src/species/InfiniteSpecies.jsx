52a9f5bcd16979efddd9730a34de8d6f28224dbb
Erich Welz <erich.welz@junglescout.com>
2022-02-10 17:13:06 -0500

    feat: first two apps completed well enough

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -9,5 +10,25 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl}) => fetchUrl(pageParam),
+    {
+      getNextPageParam: ( lastPage) => lastPage.next || undefined
+    }
+  )
+
+  if (isLoading) return <h3>Loading species yo...</h3>
+
+  return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage} >
+    {data.pages.map(pageData => 
+      pageData.results.map(spec => (
+        <Species 
+          key={spec.name}
+          name={spec.name}
+          language={spec.language}
+          averageLifespan={spec.averageLifespan}
+        />
+      )))}
+  </InfiniteScroll>
 }
